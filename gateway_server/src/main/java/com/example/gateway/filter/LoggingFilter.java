1768ac91c332f3455a40ba5ac6579bac67582ce5
biasmj016 <mjkim031@gmail.com>
2025-03-07 15:26:06 +0900

    add logging

---
+++ gateway_server/src/main/java/com/example/gateway/filter/LoggingFilter.java
@@ -0,0 +1,27 @@
+package com.example.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        logger.info("Request: {} {}", request.getMethod(), request.getURI());
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            logger.info("Response status: {}", exchange.getResponse().getStatusCode());
+        }));
+    }
+}
